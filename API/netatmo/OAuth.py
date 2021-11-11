fa6c36683d89a87dbc34afeaefe6ef64c2576727
jld5692 <jerome.ledorze@gmail.com>
2021-11-11 13:24:02 +0100

    Prise en compte du problème de variabilisation

--- API/netatmo/OAuth.py
+++ API/netatmo/OAuth.py
@@ -8,12 +8,6 @@ import config
 class Token:
     # token OAuth chez netatmo
     def get_access_token(self, CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN_URL, USERNAME, PASSWORD):
-        #CLIENT_ID = "618a158d3fd6211eb73f584f"
-        #CLIENT_SECRET = "RGKs0pJLMBHNP8mRtfbiZw7HFX4NaIuZcrAoeJb9J6GuE"
-        #ACCESS_TOKEN_URL = "https://api.netatmo.com/oauth2/token"
-        #USERNAME = "jerome.ledorze@gmail.com"
-        #PASSWORD = "Mig29Fulcrum%4"
-
         data = [
             ('grant_type', 'password'),
             ('client_id', CLIENT_ID),
