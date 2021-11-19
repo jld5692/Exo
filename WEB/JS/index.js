754b927d758301c4187e9be7037616ec64ade21d
jld5692 <jerome.ledorze@gmail.com>
2021-11-19 22:08:48 +0100

    Debut prise en main JS + divers point mineur

---
+++ WEB/JS/index.js
@@ -0,0 +1,41 @@
+// alert('Youhou man');
+// console.log("Hello");
+
+// let maVariable = "coucou";
+
+// console.log(maVariable);
+// var unTexte = "Voici un texte";
+
+// console.log(unTexte);
+// unTexte = "Nouveau Texyte ...";
+// console.log(unTexte);
+
+// const prenom = "Justine";
+// console.log(prenom);
+// prenom = "Julien";
+
+// let unCHiffre = 24;
+// unCHiffre = 12;
+// console.log(unCHiffre);
+
+// let chiffre = 24;
+// let nouvelleChaine = `Le chiffre attendu est : ${chiffre} degrés`;
+// console.log(nouvelleChaine);
+
+// Type de donnée
+// let ChaineDeCaracteres = "Je suis une chaine";
+// let chiffre = 24;
+// let boolean = true;
+// let array = ["je", "suis", 24, false];
+// let object = {
+//   prenom: "Audrey",
+//   age: 34,
+//   ville: "Lorient",
+// };
+// // console.log(typeof chiffre);
+// let arbre = null;
+
+let total = 0;
+total++;
+total -= 5;
+console.log(total);
